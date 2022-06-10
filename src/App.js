e41b0bc0d8bd3c745882dab5eb25972118ecc16e
unknown <Kakedev21.ja.catedrilla21@gmail.com>
2022-06-10 12:09:50 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./style/style.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
